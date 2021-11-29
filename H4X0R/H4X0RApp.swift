0d50eec227aa72f5273766cec88cf2c8adff35d1
mohamednasr <haloka01094519221m!>
2021-11-29 20:58:46 +0200

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Mohamed Nasr on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
